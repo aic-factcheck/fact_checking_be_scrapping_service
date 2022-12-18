97fdcb7b0e214eb8f46a9caa38bd22a20afc7665
rasto <rasto.kopal@gmail.com>
2022-12-18 21:24:46 +0100

    added cors and json api

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import trafilatura
-from flask import Flask, request
-from flask import render_template
+from flask import Flask, render_template, request
+from flask_cors import CORS
 from trafilatura.settings import use_config
 
 app = Flask(__name__)
+CORS(app)
 
-newconfig = use_config("trafilatura.cfg")
+cfg = use_config("trafilatura.cfg")
 
 @app.route("/")
 def hello_world():
@@ -16,7 +17,17 @@ def extractUrl():
     try:
         url = request.args.get('url', default="https://github.blog/2019-03-29-leader-spotlight-erin-spiceland/")
         downloaded = trafilatura.fetch_url(url)
-        result = trafilatura.extract(downloaded, config=newconfig)
+        result = trafilatura.extract(downloaded, config=cfg)
+        return result
+    except:
+        return "An error occured.."
+
+@app.route("/extract/json")
+def extractUrl():
+    try:
+        url = request.args.get('url', default="https://github.blog/2019-03-29-leader-spotlight-erin-spiceland/")
+        downloaded = trafilatura.fetch_url(url)
+        result = trafilatura.extract(downloaded, output_format="json", include_comments=False, config=cfg)
         return result
     except:
         return "An error occured.."
